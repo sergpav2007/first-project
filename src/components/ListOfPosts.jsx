ef6dbee6747d066e7b05c18d584975639080adf7
sergpav2007 <sergpav2007@gmail.com>
2023-03-27 18:17:24 +0300

    add firebase

--- src/components/ListOfPosts.jsx
+++ src/components/ListOfPosts.jsx
@@ -1,26 +1,42 @@
 import { useState, useEffect } from "react";
+import { db } from "../firebase";
+import { ref, onValue } from "firebase/database";
 
 const ListOfPosts = () => {
     const [post, setPost] = useState([]);
+    
     useEffect(() => {
-        const   url = 'http://localhost:3001/posts';
-        var responseClone;
-        const fetchData = async () => {
-            await fetch(url)
-                .then(resp => { responseClone = resp.clone(); return resp.json(); })
-                .then((json) => {
-                    const data = json.map((item, index) => ({ id: index + 1, ...item }));
-                    setPost(data);
-                },
-                    (rejectionReason) => {
-                    console.log('Error parsing JSON from response:', rejectionReason, responseClone);
-                    responseClone.text()
-                        .then((bodyText) => {
-                            console.log('Received the following instead of valid JSON:', bodyText);
-                        });
-                });
-        };
-        fetchData();
+        // const   url = 'http://localhost:3001/posts';
+        // var responseClone;
+        const docRef = ref(db, "/");
+        onValue(docRef, (snapshot) => {
+            // if (snapshot.exists()) {
+                // let arrPosts = [];
+                // snapshot.forEach(element => {
+                //     arrPosts.push({ ...element.val(), id: element.id })
+                // });
+                // setPost(arrPosts);
+                setPost(snapshot.val());
+            // }
+            // else { fetchData(); };
+        });
+        // const fetchData = async () => {
+        //     await fetch(url)
+        //         .then(resp => { responseClone = resp.clone(); return resp.json(); })
+        //         .then((json) => {
+        //             const data = json.map((item, index) => ({ id: index + 1, ...item }));
+        //             setPost(data);
+        //             // setDoc((docRef), { id: data.id, title: data.title, imageUrl: data.imageUrl, description: data.description, expertComment: data.expertComment });
+        //             // set((docRef), { id: data.id, title: data.title, imageUrl: data.imageUrl, description: data.description, expertComment: data.expertComment });
+        //         },
+        //             (rejectionReason) => {
+        //                 console.log('Error parsing JSON from response:', rejectionReason, responseClone);
+        //                 responseClone.text()
+        //                     .then((bodyText) => {
+        //                         console.log('Received the following instead of valid JSON:', bodyText);
+        //                     });
+        //             });
+        // };        
     }, []);
     return post;
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhdDHdNVjy8fnCSJyFO8s6vDnsp2lChrA",
-  authDomain: "first-easy.firebaseapp.com",
-  projectId: "first-easy",
-  storageBucket: "first-easy.appspot.com",
-  messagingSenderId: "1095691487013",
-  appId: "1:1095691487013:web:c7e75a6e301816d67efccc",
-  measurementId: "G-5LD9N6ZF51"
+  apiKey: "AIzaSyAI57hwBtOLfbsEZA0QOkdF0TVsgFF5UuI",
+  authDomain: "posts-828d1.firebaseapp.com",
+  databaseURL: "https://posts-828d1-default-rtdb.firebaseio.com",
+  projectId: "posts-828d1",
+  storageBucket: "posts-828d1.appspot.com",
+  messagingSenderId: "358361434376",
+  appId: "1:358361434376:web:a4f8a57aa430b32d1104bd"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
+export const db = getDatabase(app);
